8d69cc5301f8924773a19974172d24c5bd0c50f0
Lucas Melo <lucas.melo@foster.com.br>
2019-09-10 21:14:38 -0300

    Initial commit

---
+++ FI.AtividadeEntrevista/Util/Text.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FI.AtividadeEntrevista.Util
+{
+    public class Text
+    {
+        public static string RemoverLetras(string text)
+        {
+            Regex reg = new Regex(@"[^0-9]");
+            string ret = reg.Replace(text, string.Empty);
+            return ret;
+        }
+    }
+}
---
+++ FI.WebAtividadeEntrevista/DataAnnotations/CPFAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace FI.AtividadeEntrevista.DataAnnotations
+{
+    public class CPFAttribute : ValidationAttribute, IClientValidatable
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+                return true;
+            bool valido = Util.CPF.Validar(value.ToString());
+            return valido;
+        }
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+        {
+            yield return new ModelClientValidationRule
+            {
+                ErrorMessage = this.FormatErrorMessage(null),
+                ValidationType = "customvalidationcpf"
+            };
+        }
+    }
+}
